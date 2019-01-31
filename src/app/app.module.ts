629eff7116df323db00037519208c24677db782b
Maickel Pirchiner <maickel@geniusws.com.br>
2019-01-31 13:21:40 -0200

    Lazy do modulo de compras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,10 +17,7 @@ import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shoppin
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderComponent } from './order/order.component';
-import { OrderItensComponent } from './order/order-itens/order-itens.component'
 import { OrderService } from './order/order.service';
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule} from './shared/shared.module'
 @NgModule({
@@ -35,9 +32,6 @@ import { SharedModule} from './shared/shared.module'
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent,
-    OrderItensComponent,
-    DeliveryCostsComponent,
     OrderSummaryComponent
   ],
   imports: [
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { OrderComponent } from './order/order.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 export const ROUTES: Routes = [
@@ -17,7 +16,7 @@ export const ROUTES: Routes = [
     {path: 'menu', component: MenuComponent},
     {path: 'reviews', component: ReviewsComponent}
     ]},
-  {path: 'order', component: OrderComponent},
+  {path: 'order', loadChildren: './order/order.module#OrderModule'},
   {path: 'order-summary', component: OrderSummaryComponent},
 
 ]
